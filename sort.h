901557607affc719caa12fe4422a86d33639e15c
Beckyshee <wanjirubecky.rw@gmail.com>
2023-07-25 11:13:56 -0700

    Shell sort - Knuth Sequence

--- sort.h
+++ sort.h
@@ -35,4 +35,14 @@ void merge_sort(int *array, size_t size);
 void merge_sort_recurrsion(int *array, int *temp, int left, int right);
 void merge_sort_array(int *array, int *temp, int left, int middle, int right);
 
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void sort_deck(deck_node_t **deck);
+
 #endif
