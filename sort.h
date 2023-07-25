4223cc8088f0b87e342fd0068d99ad44afb8e052
Beckyshee <wanjirubecky.rw@gmail.com>
2023-07-25 11:18:02 -0700

    knuth sequence - shell sort

--- sort.h
+++ sort.h
@@ -1,12 +1,13 @@
 #ifndef SORT_H
 #define SORT_H
 
-#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
 
 /**
  * struct listint_s - Doubly linked list node
  *
- * @n: Integer stored in the node
+ * @n: Integer  stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
@@ -17,24 +18,15 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
+/* Prototypes for mandatory tasks */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void selection_sort(int *array, size_t size);
+size_t partition(int array[], size_t size);
+void print_sort(int array[], size_t size, int init);
 
-/* quick sort process function portotype */
-
-int Lomuto_partition(int *array, int low, int high, size_t size);
-void swap(int *array, int *i, int *j, size_t size);
-void quicksort_recurssion(int *array, int low, int high, size_t size);
-
-/* merge sorting process function prototype */
-void merge_sort(int *array, size_t size);
-void merge_sort_recurrsion(int *array, int *temp, int left, int right);
-void merge_sort_array(int *array, int *temp, int left, int middle, int right);
-
+/* Prototypes for advanced tasks */
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -43,6 +35,13 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
-#endif
+/* Prototypes for print functions */
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
+/* Prototypes for helper functions */
+void swap(int *a, int *b);
+void heapify(int *array, int end, int start, size_t size);
+
+#endif /* SORT_H */
